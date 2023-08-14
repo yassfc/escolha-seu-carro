80bec99828c40c8e7e5cc5b726ddf3576b407e85
yassfc <141974926+yassfc@users.noreply.github.com>
2023-08-14 12:58:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public class Program {
+    public static void Main() {
+
+       //Qual carro terá
+       // 0 = Ferrari
+       // 1 - Camaro
+       // 2 - Bugatti
+       // 3 - Celta
+       // 4 - Tesla
+       // 5 - Fusca
+       
+       Inicio:
+       Console.Clear();
+        int pergunta;
+        string carro = ""; 
+        string pergunta2= "";
+
+        Console.WriteLine("Escolha de 1 a 5, será seu carro");
+        pergunta = int.Parse(Console.ReadLine());
+
+        switch (pergunta) {
+            case 1: 
+                carro = "Ferrari";
+                break;
+
+            case 2:
+                carro = "Camaro";
+                break;
+
+            case 3:
+                carro = "Bugatti"; 
+                break;
+
+            case 4:
+                carro = "Tesla";
+                break;
+
+            case 5:
+                carro = "Fusca";
+                break;
+            
+            default: 
+                Console.WriteLine("Opção inválida");
+                return; 
+        }
+
+        Console.WriteLine("Seu carro será {0}", carro); 
+        Console.WriteLine(" "); 
+        Console.WriteLine("Deseja recomeçar? Sim/Não"); 
+        pergunta2 = Console.ReadLine();
+
+        if(pergunta2 == "Sim"){
+         goto Inicio;
+
+      }else{
+     Console.ReadLine();
+
+      }
+        }
+
+
+        }
+
+      
+    
+
